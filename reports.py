568a4af52cef61a2b65beaf17b2364b2da87ea57
Mishania124 <morozovmike124@gmail.com>
2022-11-18 14:36:40 +0100

    Split files

---
+++ reports.py
@@ -0,0 +1,46 @@
+import webbrowser
+import os
+
+from fpdf import FPDF
+
+
+class PdfReport:
+    """
+    Creates a Pdf file that contains data about
+    the flatmates such as their names, their due amount
+    and the period of the bill
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def gererate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add icon
+        pdf.image(name="files/R.png", w=30, h=30)
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=0, align='C', ln=1)
+
+        # Insert Period label and value
+        pdf.cell(w=100, h=40, txt='Period:', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=100, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=100, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        os.chdir('files')
+        pdf.output(self.filename)
+
+        webbrowser.open(self.filename)
