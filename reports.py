6e329cfb3dd5d17f58f9c1260dfdced6572c927e
Mishania124 <morozovmike124@gmail.com>
2022-11-22 21:03:58 +0100

    add smth

--- reports.py
+++ reports.py
@@ -54,6 +54,6 @@ class FileSharer:
         self.api_key = api_key
 
     def share(self):
-        client = Client('self.api_key')
+        client = Client(self.api_key)
         new_filelink = client.upload(filepath=self.filepath)
         return new_filelink.url
